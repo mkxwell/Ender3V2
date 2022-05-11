92ba7b20bfaf79fa83ebb926dd22e448a7310643
mkxwell <62256269+mkxwell@users.noreply.github.com>
2022-05-11 20:36:21 +0200

    Use z endstop pin as signal for controlling hotend fan mosfet

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -856,7 +856,7 @@
 // extra connectors. Leave undefined any used for non-endstop and non-probe purposes.
 #define USE_XMIN_PLUG
 #define USE_YMIN_PLUG
-#define USE_ZMIN_PLUG
+//#define USE_ZMIN_PLUG
 //#define USE_IMIN_PLUG
 //#define USE_JMIN_PLUG
 //#define USE_KMIN_PLUG
